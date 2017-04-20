d4cbd9b1578709066edf7a6e6952ea4476bc8020
xiaotianzhen <459909255@qq.com>
2017-04-20 15:45:54 +0800

    Initial commit

--- mvplogin/src/main/java/com/qianwang/mvplogin/presenter/LoginPresenter.java
+++ mvplogin/src/main/java/com/qianwang/mvplogin/presenter/LoginPresenter.java
@@ -1,7 +1,23 @@
 package com.qianwang.mvplogin.presenter;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 import com.qianwang.mvplogin.presenter.Ipresenter.ILoginPresenter;
+import com.qianwang.mvplogin.util.NetUtils;
+import com.qianwang.mvplogin.util.PhoneUtils;
 import com.qianwang.mvplogin.view.IView.ILoginView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /**
  * Created by sky on 2017/4/19.
@@ -14,29 +30,106 @@ public class LoginPresenter extends ILoginPresenter {
         mLoginView = loginView;
     }
 
-    @Override
     public boolean checkPhoneLogin(String phone, String verifyCode) {
+
+        if (PhoneUtils.isMobile(phone)) {
+
+            Pattern pattern = Pattern.compile("[0-9]*");
+            Matcher matcher = pattern.matcher(verifyCode);
+            if (matcher.matches()) {
+
+                if (NetUtils.check(mLoginView.getContext())) {
+                    return true;
+                } else {
+                    mLoginView.showMsg("当前网络不可用");
+                }
+
+            } else {
+                mLoginView.verifyCodeError("验证码格式错误");
+            }
+
+        } else {
+            mLoginView.phoneError("手机号格式错误");
+        }
+
         return false;
     }
 
     @Override
     public boolean checkUserNameLogin(String userName, String password) {
+
+        if (password.length() > 6) {
+
+            if (NetUtils.check(mLoginView.getContext())) {
+
+                return true;
+            } else {
+                mLoginView.usernameError("当前网络不可用");
+            }
+        } else {
+            mLoginView.passwordError("密码错误");
+        }
         return false;
     }
 
     @Override
-    public void phoneLogin(String phone, String verifyCode) {
+    public void phoneLogin(String phone, final String verifyCode) {
 
+        mLoginView.showLoading();
+        //判断手机号和验证码是否有效
+
+        if (checkPhoneLogin(phone, verifyCode)) {
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+
+                    if (verifyCode.equals("123456")) {
+
+                        mLoginView.loginSuccess();
+                    } else {
+                        mLoginView.loginFailed(404, "登录失败");
+                    }
+
+                }
+            }, 2000);
+        }
+        mLoginView.dismissLoading();
     }
 
     @Override
-    public void userNameLogin(String userName, String password) {
+    public void userNameLogin(final String userName, final String password) {
+        mLoginView.showLoading();
+        //判断用户名和密码是否有效
+        if (checkUserNameLogin(userName, password)) {
 
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    if (userName.equals("dong") && password.equals("1234567")) {
+                        mLoginView.loginSuccess();
+                    } else {
+                        mLoginView.loginFailed(404, "登录失败");
+                    }
+                }
+            }, 2000);
+        }
+        mLoginView.dismissLoading();
     }
 
     @Override
     public void sendVerifyCode(String phoneNum) {
 
+        if (PhoneUtils.isMobile(phoneNum)) {
+            boolean success = true;
+            if (success) {
+                mLoginView.verifyCodeSuccess(100, 200);
+            } else {
+                mLoginView.verifyCodeSuccess(100, 200);
+            }
+        }
     }
 
     @Override
--- mvplogin/src/main/java/com/qianwang/mvplogin/view/LoginActivity.java
+++ mvplogin/src/main/java/com/qianwang/mvplogin/view/LoginActivity.java
@@ -1,115 +1,248 @@
 package com.qianwang.mvplogin.view;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.qianwang.mvplogin.R;
 import com.qianwang.mvplogin.presenter.LoginPresenter;
 import com.qianwang.mvplogin.view.IView.ILoginView;
 
-public class LoginActivity extends AppCompatActivity implements ILoginView, View.OnClickListener{
+public class LoginActivity extends AppCompatActivity implements ILoginView, View.OnClickListener {
 
     private EditText et_username;
     private EditText et_pwd;
-    private Button btn_cancel;
+    private Button btn_register;
     private Button btn_login;
+    private Button btn_phoneLogin;
     private LoginPresenter mLoginPresent;
     private ProgressBar progressbar;
+    //手机验证登陆控件
+    private TextView tvVerifyCode;
+    private boolean isPhoneLogin;
+    private String verifyCode;
 
     private void initView() {
 
         et_username = (EditText) findViewById(R.id.et_username);
         et_pwd = (EditText) findViewById(R.id.et_pwd);
-        btn_cancel = (Button) findViewById(R.id.btn_cancel);
+        btn_register = (Button) findViewById(R.id.btn_register);
         btn_login = (Button) findViewById(R.id.btn_login);
+        btn_phoneLogin = (Button) findViewById(R.id.btn_phoneLogin);
         progressbar = (ProgressBar) findViewById(R.id.progressbar);
+        tvVerifyCode = (TextView) findViewById(R.id.tvVerifyCode);
 
-        btn_cancel.setOnClickListener(this);
+        btn_register.setOnClickListener(this);
         btn_login.setOnClickListener(this);
+        btn_phoneLogin.setOnClickListener(this);
+        tvVerifyCode.setOnClickListener(this);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initView();
-        mLoginPresent=new LoginPresenter(this);
+        mLoginPresent = new LoginPresenter(this);
+        userNameLoginViewInit();
+    }
+
+    /**
+     * 手机登录初始化
+     */
+
+    public void userNameLoginViewInit() {
+
+        isPhoneLogin = false;
+        tvVerifyCode.setVisibility(View.GONE);
+        AlphaAnimation animation = new AlphaAnimation(1.0f, 0.0f);
+        animation.setDuration(2000);
+        tvVerifyCode.setAnimation(animation);
+        et_username.setInputType(EditorInfo.TYPE_CLASS_TEXT);
+        et_username.setText("");
+        et_pwd.setText("");
+        btn_phoneLogin.setText("手机号登录");
+        et_username.setHint("请输入用户名");
+        et_pwd.setHint("请输入密码");
+
+        btn_phoneLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phoneLoginViewInit();
+            }
+        });
+
+
+    }
+
+    public void phoneLoginViewInit() {
+        isPhoneLogin = true;
+        tvVerifyCode.setVisibility(View.VISIBLE);
+        AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f);
+        animation.setDuration(2000);
+        tvVerifyCode.setAnimation(animation);
+        et_username.setInputType(EditorInfo.TYPE_CLASS_PHONE);
+        et_username.setText("");
+        et_pwd.setText("");
+        btn_phoneLogin.setText("用户登录");
+        et_username.setHint("请输入手机号");
+        et_pwd.setHint("请输入验证码");
+
+        btn_phoneLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userNameLoginViewInit();
+            }
+        });
     }
 
     @Override
     public void loginSuccess() {
-        Toast.makeText(getContext(),"登录成功",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "登录成功", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void loginFailed(int status, String msg) {
-        Toast.makeText(getContext(),"登录失败",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), status+msg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void usernameError(String errorMsg) {
-        et_username.setError("用户名输入错误");
+        et_username.setError(errorMsg);
         et_username.requestFocus();
     }
 
     @Override
     public void phoneError(String errorMsg) {
-
+        et_username.setError(errorMsg);
+        et_username.requestFocus();
     }
 
     @Override
     public void passwordError(String errorMsg) {
-          et_pwd.setError("密码输入错误");
+
+        et_pwd.setError(errorMsg);
     }
 
     @Override
     public void verifyCodeError(String errorMsg) {
 
+        showMsg(errorMsg);
     }
 
     @Override
     public void verifyCodeFailed(String errorMsg) {
-
+        showMsg(errorMsg);
     }
 
     @Override
     public void verifyCodeSuccess(int reaskDuration, int expireDuration) {
-
+        showMsg("注册短信下发，验证码 " + expireDuration / 60 + " 分钟内有效！");
     }
 
     @Override
     public void showLoading() {
-          progressbar.setVisibility(View.VISIBLE);
+        showLoading(true);
     }
 
     @Override
     public void dismissLoading() {
-        progressbar.setVisibility(View.GONE);
+        showLoading(false);
 
     }
+
+    /**
+     * 加载的时候界面控件的显示和隐藏
+     *
+     * @param ishow
+     */
+
+    private void showLoading(boolean ishow) {
+
+        if (ishow) {
+            progressbar.setVisibility(View.VISIBLE);
+            btn_login.setEnabled(false);
+            btn_phoneLogin.setEnabled(false);
+            btn_register.setEnabled(false);
+            et_pwd.setEnabled(false);
+            et_username.setEnabled(false);
+
+        } else {
+            progressbar.setVisibility(View.GONE);
+            btn_login.setEnabled(true);
+            btn_phoneLogin.setEnabled(true);
+            btn_register.setEnabled(true);
+            et_pwd.setEnabled(true);
+            et_username.setEnabled(true);
+
+        }
+    }
+
 
     @Override
     public void showMsg(String msg) {
 
+        Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void showMsg(int msgId) {
-
+        showMsg(msgId);
     }
 
     @Override
     public Context getContext() {
-        return null;
+        return this;
     }
 
     @Override
     public void onClick(View view) {
 
+
+        switch (view.getId()) {
+            case R.id.btn_login:
+
+                if(isPhoneLogin){
+                    mLoginPresent.phoneLogin(et_username.getText().toString(),et_pwd.getText().toString());
+                }else {
+                    mLoginPresent.userNameLogin(et_username.getText().toString(),et_pwd.getText().toString());
+                }
+                break;
+            case R.id.tvVerifyCode:
+
+                mLoginPresent.sendVerifyCode(et_username.getText().toString());
+
+                final EditText editText=new EditText(getContext());
+                new AlertDialog.Builder(getContext()).setTitle("验证码").setMessage("请输入短信获取到的验证码").setView(editText)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                verifyCode=editText.getText().toString();
+
+                                et_pwd.setText(verifyCode);
+                            }
+                        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).show();
+                break;
+            case R.id.btn_register:
+
+                break;
+
+        }
     }
 }
